ea0dbd2f2fd7ab71d4cba551209501dee9f3639e
yovjvng <yoyo0249@naver.com>
2023-04-10 18:10:17 +0900

    23.04.10

---
+++ src/board_list.php
@@ -0,0 +1,100 @@
+<?php
+    define( "DOC_ROOT", $_SERVER["DOCUMENT_ROOT"]."/" );
+    define( "URL_DB", DOC_ROOT."mini_board/src/common/db_common.php" );
+    include_once( URL_DB );
+
+    if ( array_key_exists( "page_num", $_GET ) ) 
+    {
+        $page_num = $_GET["page_num"];
+    }
+    else
+    {
+        $page_num = 1;
+    }
+
+
+    // $arr_get = $_GET;
+
+    $limit_num = 5;
+
+    // $page_num = $arr_get["page_num"];
+    // 게시판 정보 테이블 전체 카운트 획득
+    $result_cnt = select_board_info_cnt();
+
+    // max page number
+    $max_page_num = ceil( (int)$result_cnt[0]["cnt"] / $limit_num );
+
+    // offset
+    $offset = ( $page_num * $limit_num ) - $limit_num;
+
+    $arr_prepare = 
+        array(
+            "limit_num" => $limit_num
+            ,"offset"   => $offset
+        );
+    $result_paging = select_board_info_paging( $arr_prepare );
+    
+    // print_r( $result_cnt );
+    // print_r( $max_page_num );
+    // var_dump( $max_page_num );
+    // var_dump( $result_cnt );
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./board.css">
+    <title>게시판</title>
+    <style>
+        
+        .btn
+        {
+            margin : 0 auto;
+        }
+    </style>
+</head>
+<body>
+    
+    <table class='table table-striped'>
+        <thead>
+            <tr>
+                <th>게시물 번호</th>
+                <th>게시물 제목</th>
+                <th>작성일자</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach( $result_paging as $recode )
+                {
+            ?>
+                    <tr>
+                        <td><?php echo $recode["board_no"] ?></td>
+                        <td><?php echo $recode["board_title"] ?></td>
+                        <td><?php echo $recode["board_write_date"] ?></td>
+                    </tr>
+            <?php
+                }
+            ?>
+          
+        </tbody>
+    </table>
+    <div class="btn_con">
+    <?php
+        for( $i = 1; $i <= $max_page_num; $i++ )
+        {
+    ?>       
+            <a class="btn btn-outline-secondary" href='board_list.php?page_num=<?php echo $i ?>'><?php echo $i ?></a>
+    <?php
+        }
+    ?>
+    </div>
+</body>
+</html>
---
+++ src/common/db_common.php
@@ -0,0 +1,126 @@
+<?php
+
+function db_conn( &$param_conn )
+{
+    $host = "localhost";
+    $user = "root";
+    $pass = "root506";
+    $charset = "utf8mb4";
+    $db_name = "board";
+    $dns = "mysql:host=".$host.";dbname=".$db_name.";charset=".$charset;
+    $pdo_option = 
+        array(
+            PDO::ATTR_EMULATE_PREPARES      => false
+            ,PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION
+            ,PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC
+        );
+        
+    try
+    {
+        $param_conn = new PDO( $dns, $user, $pass, $pdo_option );
+
+    }
+    catch( Exception $e )
+    {
+        $param_conn = null;
+        throw new Exception( $e->getMessage() );
+    }
+
+}
+
+function select_board_info_paging( &$param_arr )
+{
+    $sql = 
+        " SELECT "
+        . " board_no "
+        . " , board_title "
+        . " , board_write_date "
+        . " FROM "
+        . " board_info "
+        . " WHERE "
+        . " board_del_flg = '0' "
+        . " ORDER BY "
+        . " board_no DESC "
+        . " LIMIT :limit_num OFFSET :offset "
+        ;
+
+        $arr_prepare = 
+            array(
+                ":limit_num"    => $param_arr["limit_num"]
+                , ":offset"      => $param_arr["offset"]
+            );
+
+        $conn = null;
+        try
+        {
+            db_conn( $conn );
+            $stmt = $conn->prepare( $sql );
+            $stmt->execute( $arr_prepare );
+            $result = $stmt->fetchAll();
+        } 
+        catch( Exception $e )
+        {
+            return false;
+        }
+        finally
+        {
+            $conn = null;
+        }
+
+        return $result;
+}
+
+function select_board_info_cnt()
+{
+    $sql =
+        " SELECT "
+        ." COUNT(*) cnt"
+        ." FROM "
+        ." board_info "
+        ." WHERE "
+        ." board_del_flg = '0' "
+        ;
+
+        $arr_prepare = array();
+
+        $conn = null;
+        try
+        {
+            db_conn( $conn );
+            $stmt = $conn->prepare( $sql );
+            $stmt->execute( $arr_prepare );
+            $result = $stmt->fetchAll();
+        } 
+        catch( Exception $e )
+        {
+            return false;
+        }
+        finally
+        {
+            $conn = null;
+        }
+
+        return $result;
+
+}
+
+
+
+
+// TODO : test Start
+// $arr = 
+//     array(
+//         "limit_num" =>5
+//         , "offset"    =>0
+//     );
+// $result = select_board_info_paging( $arr );
+
+// print_r( $result );
+// TODO : test End
+
+
+
+
+
+
+?>
